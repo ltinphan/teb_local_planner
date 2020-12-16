95438fa02cbb5c12c4991e31fb5c3b8c43565cd2
rdeniza <deniz.aksoy@github.com>
2020-12-16 15:19:52 +0100

    Removed robot is out of bounds warning.

--- teb_local_planner/src/teb_local_planner_ros.cpp
+++ teb_local_planner/src/teb_local_planner_ros.cpp
@@ -403,10 +403,10 @@ geometry_msgs::msg::TwistStamped TebLocalPlannerROS::computeVelocityCommands(
   // Always true since we do not use costmap. isTrajectoryFeasible only uses costmap to check feasibility.
   // Therefore we do not need to check whether trajectory feasible or not.
   bool feasible;
-//  planner_->isTrajectoryFeasible(costmap_model_.get(), footprint_spec_, robot_inscribed_radius_, robot_circumscribed_radius, cfg_->trajectory.feasibility_check_no_poses);
-//    if (!feasible)
-//    {
-//        RCLCPP_ERROR(nh_->get_logger(), "Path is infeasible, but we will disregard that");
+  planner_->isTrajectoryFeasible(costmap_model_.get(), footprint_spec_, robot_inscribed_radius_, robot_circumscribed_radius, cfg_->trajectory.feasibility_check_no_poses);
+    if (!feasible)
+    {
+        RCLCPP_ERROR(nh_->get_logger(), "Path is infeasible, but we will disregard that.");
 //      cmd_vel.twist.linear.x = cmd_vel.twist.linear.y = cmd_vel.twist.angular.z = 0;
 //
 //      // now we reset everything to start again with the initialization of new trajectories.
@@ -419,7 +419,7 @@ geometry_msgs::msg::TwistStamped TebLocalPlannerROS::computeVelocityCommands(
 //      throw nav2_core::PlannerException(
 //        std::string("TebLocalPlannerROS: trajectory is not feasible. Resetting planner...")
 //      );
-//    }
+    }
 
   // Get the velocity command for this sampling interval
   if (!planner_->getVelocityCommand(cmd_vel.twist.linear.x, cmd_vel.twist.linear.y, cmd_vel.twist.angular.z, cfg_->trajectory.control_look_ahead_poses))
