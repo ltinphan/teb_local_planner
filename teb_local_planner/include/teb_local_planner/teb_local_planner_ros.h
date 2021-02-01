805d02103927c628ff01e249722b4e102baeb99c
rdeniza <deniz.aksoy@logivations.com>
2021-02-01 14:00:33 +0100

    added custon narrow isle obstacles

--- teb_local_planner/include/teb_local_planner/teb_local_planner_ros.h
+++ teb_local_planner/include/teb_local_planner/teb_local_planner_ros.h
@@ -239,6 +239,13 @@ protected:
     * @param obst_msg pointer to the message containing a list of polygon shaped obstacles
     */
   void customObstacleCB(const costmap_converter_msgs::msg::ObstacleArrayMsg::ConstSharedPtr obst_msg);
+
+
+    /**
+     * @brief Callback for custom obstacles that are not obtained from the costmap
+     * @param obst_msg pointer to the message containing a list of polygon shaped obstacles
+     */
+    void customNarrowObstacleCB(const costmap_converter_msgs::msg::ObstacleArrayMsg::ConstSharedPtr obst_msg);
   
    /**
     * @brief Callback for custom via-points
@@ -388,7 +395,10 @@ private:
   //std::shared_ptr< dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig> > dynamic_recfg_; //!< Dynamic reconfigure server to allow config modifications at runtime
   rclcpp::Subscription<costmap_converter_msgs::msg::ObstacleArrayMsg>::SharedPtr custom_obst_sub_; //!< Subscriber for custom obstacles received via a ObstacleMsg.
   std::mutex custom_obst_mutex_; //!< Mutex that locks the obstacle array (multi-threaded)
+  std::mutex custom_narrow_obst_mutex_; //!< Mutex that locks the obstacle array (multi-threaded)
   costmap_converter_msgs::msg::ObstacleArrayMsg custom_obstacle_msg_; //!< Copy of the most recent obstacle message
+
+  costmap_converter_msgs::msg::ObstacleArrayMsg custom_narrow_obstacle_msg_; //!< Copy of the most recent obstacle message
 
   rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr via_points_sub_; //!< Subscriber for custom via-points received via a Path msg.
   bool custom_via_points_active_; //!< Keep track whether valid via-points have been received from via_points_sub_
