51d4af9300cb98fa07d04a223d389784a5593096
liyuanlogi <li-yuan.hsu@logivations.com>
2021-11-25 16:16:51 +0100

    fix feasibilitz check

--- teb_local_planner/src/homotopy_class_planner.cpp
+++ teb_local_planner/src/homotopy_class_planner.cpp
@@ -634,7 +634,7 @@ int HomotopyClassPlanner::bestTebIdx() const
 int HomotopyClassPlanner::isTrajectoryFeasible(dwb_critics::ObstacleFootprintCritic* costmap_model, const std::vector<geometry_msgs::msg::Point>& footprint_spec,
                                                 double inscribed_radius, double circumscribed_radius, int look_ahead_idx)
 {
-  int feasible = -1;
+  int feasible = 0;
   while(rclcpp::ok() && !feasible && tebs_.size() > 0)
   {
     TebOptimalPlannerPtr best = findBestTeb();
@@ -644,7 +644,7 @@ int HomotopyClassPlanner::isTrajectoryFeasible(dwb_critics::ObstacleFootprintCri
       return 0;
     }
     feasible = best->isTrajectoryFeasible(costmap_model, footprint_spec, inscribed_radius, circumscribed_radius, look_ahead_idx);
-    if(!feasible)
+    if(feasible >= 0)
     {
       removeTeb(best);
       if(last_best_teb_ && (last_best_teb_ == best)) // Same plan as before.
