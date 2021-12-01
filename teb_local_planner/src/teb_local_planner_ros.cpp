d12092c3f75f20c56a84b3b936378b0e6cc6a6ff
liyuanlogi <li-yuan.hsu@logivations.com>
2021-12-01 13:45:58 +0100

    check no teb obstacle received for all types of obstacles

--- teb_local_planner/src/teb_local_planner_ros.cpp
+++ teb_local_planner/src/teb_local_planner_ros.cpp
@@ -388,11 +388,16 @@ geometry_msgs::msg::TwistStamped TebLocalPlannerROS::computeVelocityCommands(
   // also consider custom obstacles (must be called after other updates, since the container is not cleared)
   updateObstacleContainerWithCustomObstacles();
 
-  if (!custom_obstacle_msg_.obstacles.empty()){
-    float seconds_diff = nh_->now().seconds() - custom_obstacle_msg_.obstacles.at(0).header.stamp.sec;
+  if (!custom_obstacle_msg_.obstacles.empty() || !custom_narrow_obstacle_msg_.empty() || !custom_static_obstacle_msg_.empty()){
+    float obst_seconds_diff = nh_->now().seconds() - custom_obstacle_msg_.obstacles.at(0).header.stamp.sec;
+    float narrow_obst_seconds_diff = nh_->now().seconds() - custom_narrow_obstacle_msg_.obstacles.at(0).header.stamp.sec;
+    float static_obst_seconds_diff = nh_->now().seconds() - custom_static_obstacle_msg_.obstacles.at(0).header.stamp.sec;
+
     // TODO: 5 seconds?
-    if (seconds_diff > 5){
-      RCLCPP_INFO(nh_->get_logger(), "Diff %f", seconds_diff);
+    if (obst_seconds_diff > 5 && narrow_obst_seconds_diff > 5 && static_obst_seconds_diff > 5){
+      RCLCPP_INFO(nh_->get_logger(), "No new custom obstacles received for %fs", obst_seconds_diff);
+      RCLCPP_INFO(nh_->get_logger(), "No new custom narrow obstacles received for %fs", narrow_obst_seconds_diff);
+      RCLCPP_INFO(nh_->get_logger(), "No custom static obstacles received for %fs", static_obst_seconds_diff);
       RCLCPP_ERROR(nh_->get_logger(), "No new obstacles were received for the past 5 seconds");
       cmd_vel.twist.linear.x = cmd_vel.twist.linear.y = cmd_vel.twist.angular.z = 0;
       last_cmd_ = cmd_vel.twist;
